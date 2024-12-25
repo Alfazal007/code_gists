8e6d19e73ef492afa1386a425ac8f08a3a88d35b
Alfazal007 <alfazal7777@gmail.com>
2024-12-26 02:50:10 +0530

    deply

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import { app } from "./app";
 import { envVariables } from "./config/envVariables";
 
+app.get('/', (req, res) => {
+	res.send('test');
+});
+
 app.listen(envVariables.port, () => {
 	console.log(`App listening on port ${envVariables.port}`)
 });
